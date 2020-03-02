18c4a8a8dbb4ef9b8ae7d9522b8cc71a5c48bb1f
flb <f.lb@gmx.com>
2020-03-02 21:47:10 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# 4chan Mass Quotes browser extension
+
+This browser extension aims to facilitate the creation of mass replies on 4chan.org and 4channel.org. It may or may not work on other imageboards.
+
+## Usage
+
+Click on the toolbar icon when in a thread, and select one of two options. "Quote" quotes all the replies in the thread, and "Sneed" adds "sneed" at the end. "Copy To Clipboard" is self explanatory.
+
+Tested on Firefox 73 for GNU/Arch Loonix, Microsoft Winblows 10 and Android 9
+
+### Buy me a coffee?
+
+https://ko-fi.com/hamletdufromage
---
+++ popup/chooseMode.js
@@ -0,0 +1,80 @@
+// fill the <p> with the quotes
+function fillField(textData){
+    // The innerHTML is only done on the popup, not on actual webpages
+    document.getElementById("textField").innerHTML = textData;
+}
+
+function listenForClicks() {
+    document.addEventListener("click", (e) => {
+
+        function updateClipboard() {
+            var newClip = document.getElementById("textField").innerText;
+
+            if (newClip !== "" && newClip !== "sneed"){
+                navigator.clipboard.writeText(newClip);
+            }
+            /*
+            //navigator.permissions.query({name: "clipboard-write"}).then(result => {
+                //if (result.state == "granted" || result.state == "prompt") {
+                    navigator.clipboard.writeText(newClip).then(function() {
+                        
+                    }, function() {
+                        
+                    });
+                //}
+            //});
+            */
+        }
+        
+        function copyQuote() {
+            copyToClipBoard("lol");
+        }
+
+        function hide(tabs) {
+            browser.tabs.insertCSS({code: hidePage});
+        }
+        function massQuote(tabs) {
+            // send a message with the kind of quote that must be performed
+            browser.tabs.sendMessage(tabs[0].id, {
+                command: "massQuote",
+                action: e.target.textContent
+            });
+
+        }
+        function reportError(error) {
+            console.error(`Could not mass quote: ${error}`);
+        }
+
+        // Check if buttons are clicked
+        if (e.target.classList.contains("action")) {
+            
+            browser.tabs.query({active: true, currentWindow: true})
+              .then(massQuote)
+              .catch(reportError);
+        }
+
+        else if (e.target.classList.contains("copy")) {
+            browser.tabs.query({active: true, currentWindow: true})
+              .then(updateClipboard)
+              .catch(reportError);
+        }
+    });
+}
+
+function reportExecuteScriptError(error) {
+    document.querySelector("#popup-content").classList.add("hidden");
+    document.querySelector("#error-content").classList.remove("hidden");
+    console.error(`Failed to execute bmassQuote content script: ${error.message}`);
+}
+
+
+browser.tabs.executeScript({file: "/content_scripts/massQuote.js"})
+.then(listenForClicks)
+.catch(reportExecuteScriptError);
+
+function handleMessage(message, sender, sendResponse){
+    fillField(message); 
+  }
+
+// get the message from contents
+browser.runtime.onMessage.addListener(handleMessage)
