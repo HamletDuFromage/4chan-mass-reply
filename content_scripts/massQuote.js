4f8241ceffc2f4c538bc5d68a2c40f8e39633dd4
flb <f.lb@gmx.com>
2020-03-04 21:21:31 +0100

    Added support for >101 replies posts

--- content_scripts/massQuote.js
+++ content_scripts/massQuote.js
@@ -2,19 +2,39 @@
 (function() {
 
     browser.runtime.onMessage.addListener((message) => {
-        
         var str = "";
-        if (message.command === "massQuote") {
-            
-            var posts = document.getElementsByClassName("postContainer");
-            for (var i = 0; i < posts.length; i++) {
-                str += ">>" + posts[i].id.substring(2) + "<br>";
-            
+        var url = window.location.href;
+        if(url.match(/boards.4chann?e?l?.org\/[a-z]+\/thread/))
+        {
+            if (message.command === "massQuote") {
+                var maxLines = 100;
+                var posts = document.getElementsByClassName("postContainer");
+                var quotesNumber = Math.min(posts.length, 220);
+                var cols = Math.floor(quotesNumber/maxLines);
+
+                if (cols < 1){
+                    for (var i = 0; i < quotesNumber; i++) {
+                        str += ">>" + posts[i].id.substring(2) + "<br>";
+                    }
+                }
+                else{
+                    var r = Math.floor((quotesNumber - maxLines)/2) + 1
+                    var r = 30;
+                    for (var i = 0; i < maxLines - r; i++) {
+                        str += ">>" + posts[i].id.substring(2) + "<br>";
+                    }
+                    for (var i = 0; i < quotesNumber - maxLines - r; i++){
+                        str += ">>" + posts[i].id.substring(2);
+                        if (i%(cols+1) === 0 || i === quotesNumber - maxLines - r - 1) {str += "<br>";}
+                        else{str += " ";}
+                    }
+                }
             }
-        } 
-        if(message.action === "Sneed"){
-            str+= "sneed";
+            if(message.action === "Sneed"){
+                str+= "sneed";
+            }
         }
+        else{str += "This is not a recongized 4chan thread";}
         browser.runtime.sendMessage(str);  
       });
 
