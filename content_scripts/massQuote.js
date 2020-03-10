7aa319e29f79c4a7af0333831d365a22ca6ddd69
flb <f.lb@gmx.com>
2020-03-10 18:50:52 +0100

    Added possibility to chose between quoting top or bottom posts

--- content_scripts/massQuote.js
+++ content_scripts/massQuote.js
@@ -2,15 +2,20 @@
 (function() {
 
     browser.runtime.onMessage.addListener((message) => {
+        var maxLines = 100;
         var str = "";
         var url = window.location.href;
         if(url.match(/boards.4chann?e?l?.org\/[a-z]+\/thread/))
         {
+            var quotesNumber = 166;
+            if(url.match(/\/jp/)){maxQuotes = 416;}
+            if(url.match(/\/lit/)){maxQuotes = 250;}
+
             if (message.command === "massQuote") {
-                var maxLines = 100;
                 var posts = document.getElementsByClassName("postContainer");
-                var quotesNumber = Math.min(posts.length, 220);
+                quotesNumber = Math.min(posts.length, quotesNumber);
                 var cols = Math.floor(quotesNumber/maxLines);
+                var offset = message.bttm * (posts.length - quotesNumber);
 
                 if (cols < 1){
                     for (var i = 0; i < quotesNumber; i++) {
@@ -18,14 +23,13 @@
                     }
                 }
                 else{
-                    var r = Math.floor((quotesNumber - maxLines)/2) + 1
-                    var r = 30;
-                    for (var i = 0; i < maxLines - r; i++) {
+                    var r = Math.floor((quotesNumber - maxLines)/cols) + 1;
+                    for (var i = 0 + offset; i < maxLines - r + offset; i++) {
                         str += ">>" + posts[i].id.substring(2) + "<br>";
                     }
-                    for (var i = maxLines - r; i < quotesNumber; i++){
+                    for (var i = maxLines - r + offset; i < quotesNumber + offset; i++){
                         str += ">>" + posts[i].id.substring(2);
-                        if (i%(cols+1) === 0 || i === quotesNumber - maxLines - r - 1) {str += "<br>";}
+                        if ((i - maxLines - r + offset)%(cols+1) === 0 || i === quotesNumber - 1) {str += "<br>";}
                         else{str += " ";}
                     }
                 }
