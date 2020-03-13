b2f36cf3059abf84932ea9bea8ead2b9a185ef9d
flb <f.lb@gmx.com>
2020-03-13 01:10:46 +0100

    Added button to quote dubs
    Added local save of parameters
    
    Added local save of parameters
    
    Added local save of parameters

--- content_scripts/massQuote.js
+++ content_scripts/massQuote.js
@@ -8,8 +8,8 @@
         if(url.match(/boards.4chann?e?l?.org\/[a-z]+\/thread/))
         {
             var quotesNumber = 166;
-            if(url.match(/\/jp/)){maxQuotes = 416;}
-            if(url.match(/\/lit/)){maxQuotes = 250;}
+            if(url.match(/\/jp/)){quotesNumber = 416;}
+            if(url.match(/\/lit/)){quotesNumber = 250;}
 
             if (message.command === "massQuote") {
                 var posts = document.getElementsByClassName("postContainer");
@@ -29,7 +29,7 @@
                     }
                     for (var i = maxLines - r + offset; i < quotesNumber + offset; i++){
                         str += ">>" + posts[i].id.substring(2);
-                        if ((i - maxLines - r + offset)%(cols+1) === 0 || i === quotesNumber - 1) {str += "<br>";}
+                        if ((i - maxLines - r + offset)%(cols+1) === 0 || i === quotesNumber + offset - 1) {str += "<br>";}
                         else{str += " ";}
                     }
                 }
