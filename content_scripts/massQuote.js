cef08ce4a55916f6e1c8293005cf53e8a40919c5
flb <f.lb@gmx.com>
2020-09-04 00:35:13 +0200

    Added copypasta selection

--- content_scripts/massQuote.js
+++ content_scripts/massQuote.js
@@ -1,3 +1,32 @@
+function loadShitposts(){
+    return [
+        {
+            "name" : "0-Interjection",
+            "content" : "I\'d just like to interject for a moment.  What you\'re referring to as Linux,<br>is in fact, GNU/Linux, or as I\'ve recently taken to calling it, GNU plus Linux.<br>Linux is not an operating system unto itself, but rather another free component<br>of a fully functioning GNU system made useful by the GNU corelibs, shell<br>utilities and vital system components comprising a full OS as defined by POSIX.<br><br>Many computer users run a modified version of the GNU system every day,<br>without realizing it.  Through a peculiar turn of events, the version of GNU<br>which is widely used today is often called \"Linux\", and many of its users are<br>not aware that it is basically the GNU system, developed by the GNU Project.<br><br>There really is a Linux, and these people are using it, but it is just a<br>part of the system they use.  Linux is the kernel: the program in the system<br>that allocates the machine\'s resources to the other programs that you run.<br>The kernel is an essential part of an operating system, but useless by itself;<br>it can only function in the context of a complete operating system.  Linux is<br>normally used in combination with the GNU operating system: the whole system<br>is basically GNU with Linux added, or GNU/Linux.  All the so-called \"Linux\"<br>distributions are really distributions of GNU/Linux.<br>"
+        },
+        {
+            "name" : "1-No Richard",
+            "content" : "No, Richard, it\'s \'Linux\', not \'GNU/Linux\'. The most important contributions that the FSF made to Linux were the creation of the GPL and the GCC compiler. Those are fine and inspired products. GCC is a monumental achievement and has earned you, RMS, and the Free Software Foundation countless kudos and much appreciation.<br><br>Following are some reasons for you to mull over, including some already answered in your FAQ.<br><br>One guy, Linus Torvalds, used GCC to make his operating system (yes, Linux is an OS -- more on this later). He named it \'Linux\' with a little help from his friends. Why doesn\'t he call it GNU/Linux? Because he wrote it, with more help from his friends, not you. You named your stuff, I named my stuff -- including the software I wrote using GCC -- and Linus named his stuff. The proper name is Linux because Linus Torvalds says so. Linus has spoken. Accept his authority. To do otherwise is to become a nag. You don\'t want to be known as a nag, do you?<br><br>(An operating system) != (a distribution). Linux is an operating system. By my definition, an operating system is that software which provides and limits access to hardware resources on a computer. That definition applies wherever you see Linux in use. However, Linux is usually distributed with a collection of utilities and applications to make it easily configurable as a desktop system, a server, a development box, or a graphics workstation, or whatever the user needs. In such a configuration, we have a Linux (based) distribution. Therein lies your strongest argument for the unwieldy title \'GNU/Linux\' (when said bundled software is largely from the FSF). Go bug the distribution makers on that one. Take your beef to Red Hat, Mandrake, and Slackware. At least there you have an argument. Linux alone is an operating system that can be used in various applications without any GNU software whatsoever. Embedded applications come to mind as an obvious example."
+        },
+        {
+            "name" : "2-No one:",
+            "content" : "no one:<br>absolutely nobody:<br>still nobody:<br>not a single soul:<br>literally no one:<br>not even big chungus:<br>kiwicel: *makes retarded post*<br>leftypol: I\'m going to end this man\'s whole career<br>[everyone disliked that]<br>baby yeed: wait that\'s illegal<br>brie larson: ok that was lowkey on point<br>pickle rick: *slaps roof of car* luke did i ever tell you about the time i turned myself into a pickle? it was an epic moment.<br>luke: *is retarded*<br>CIA: Bane?<br>sans undertale: hey don\'t google HP Lovecraft\'s cat name<br>[OP googles hp lovecrafts cat name]<br>CIA: congratulations you got yourself caught!<br>stan lee: *flies past in a spaceship* ooooh i dont care what universe you\'re from that\'s GOTTA HURT<br>[everyone laughed]<br>keanu reeves: you\'re breathtaking!<br>area 51 guards:i bet i can take on keanu reeves<br>keanu reeves: you sure about that<br>keanu reeves: kills all area 51 guards<br>area 51:wait thats illegal<br>[Everyone liked that]<br>CIA: am I a joke to you?<br>Alt right incels: there\'s no way star wars can be good agai....<br>Baby Yeed: hold my beer<br>Big chungus joined the chat<br>Drumpf has left the chat<br>Markiplier: Welcome To The Dark Side Of Youtube<br>4chanlets: \'Yeah, I\'m thinking this is kind of epic based pilled, maybe a bit of a coom moment?? Idk think I might post a frog."
+        },
+        {
+            "name" : "3-Cucked",
+            "content" : "Is having daughters the ultimate cuckoldry?<br><br>I cannot think or comprehend of anything more cucked than having a daughter. Honestly, think about it rationally. You are feeding, clothing, raising and rearing a girl for at least 18 years solely so she can go and get ravaged by another man. All the hard work you put into your beautiful little girl - reading her stories at bedtime, making her go to sports practice, making sure she had a healthy diet, educating her, playing with her. All of it has one simple result: her body is more enjoyable for the men that will eventually fuck her in every hole.<br><br>Raised the perfect girl? Great. Who benefits? If you\'re lucky, a random man who had nothing to do with the way she grew up, who marries her. He gets to fuck her tight pussy every night. He gets the benefits of her kind and sweet personality that came from the way you raised her.<br><br>As a man who has a daughter, you are LITERALLY dedicating at least 20 years of your life simply to raise a girl for another man to enjoy. It is the ULTIMATE AND FINAL cuck. Think about it logically"
+        },
+        {
+            "name" : "4-India",
+            "content" : "America: The question\'s wrong!!<br><br>UK: Rubbish !!<br><br>India: F(IV)E This is the reason you find Indians everywhere in the world in finance, business, medicine, engineering & arts... anything to do with optimising your brain!!<br><br>British: Can u Swim?<br>Indian: No<br>British: Then a Dog is Better den u because It Swims. Indian: Can u Swim?<br>British: Yes!<br>Indian: Then What\'s the Difference between u & Dog… British Shocked,Faints!! Indian Rocks!<br><br>European : Y do U indians come in all colors, look at us,we R all white..?<br><br>Abdul Kalam: Horses too come in different colors but donkeys R all the same..!!!<br><br>Einstein & a Indian sitting next to each other on a long flight...<br><br>Einstein says:<br>\"Let\'s play a game...<br>I will ask you a question,<br>if you don\'t know the answer,<br>you pay me only $5<br>and<br>if I don\'t know the answer,<br>I will pay you $500...\"<br><br>Einstein asks the first question:<br>What\'s the distance from the Earth to the Moon...?<br><br>Indian doesn\'t say a word,<br>Reaches his pocket,<br>Pulls out a $5...<br><br>Now...<br>It\'s the indian turn...<br><br>He asks Einstein:<br>What goes up a hill with 3 legs<br>and<br>comes down on 4 legs..?<br><br>Einstein searches the net and asks all his smart friends...<br>After an hour he gives Indian $500...<br><br>Einstein going nuts and asks:<br>Well...<br>so what goes up a hill with three legs and comes down with four..?<br><br>Indian reaches his pocket and gives Einstein $5...<br><br>Einstein fainted.....<br><br>Send to all Indians all over the globe! Magic Missing Indian flag in this? Don\'t worry send this message to only 2 groups u will shock to see all these flags will become Indian flags Created by: engineering students Of CIT"
+        },
+        {
+            "name" : "contribute",
+            "content" : "please contribute copypastas to to https://github.com/HamletDuFromage/4chan-mass-quote/issues"
+        }
+    ];
+}
+
 
 (function() {
 
@@ -6,18 +35,28 @@
             return;
         }
 
-        var maxLines = 100;
         var str = "";
         var url = window.location.href;
-        var posts = document.getElementsByClassName("postContainer");
-        var postLength = posts[0].id.length;
-        var quotesNumber = 166;
-        if(url.match(/\/jp/)){quotesNumber = 416;}
-        if(url.match(/\/lit/)){quotesNumber = 250;}
-        quotesNumber = Math.min(posts.length, quotesNumber);
 
-        if(url.match(/boards.4chann?e?l?.org\/[a-z]+\/thread/))
+        function reportExecuteScriptError(error) {
+            console.error(`Failed to execute massQuote content script: ${error.message}`);
+        }
+
+
+        if(message.action === "Select"){
+            str+=loadShitposts()[message.selected].content;
+        }
+
+        else if(url.match(/boards.4chann?e?l?.org\/[a-z]+\/thread/))
         {
+            var maxLines = 100;
+            var posts = document.getElementsByClassName("postContainer");
+            var postLength = posts[0].id.length;
+            var quotesNumber = 166;
+            if(url.match(/\/jp/)){quotesNumber = 416;}
+            if(url.match(/\/lit/)){quotesNumber = 250;}
+            quotesNumber = Math.min(posts.length, quotesNumber);
+            
             if(message.action != "Check 'em")
             {
                 var cols = Math.floor(quotesNumber/maxLines);
@@ -50,10 +89,10 @@
                         str += ">>" + posts[i].id.substring(2, postLength) + "<br>";
                     }
                 }
-            }
+            } 
         }
         else{str += "This is not a recongized 4chan thread";}
-        browser.runtime.sendMessage(str);  
+        if(str != ""){browser.runtime.sendMessage(str);}
       });
 
 })();
