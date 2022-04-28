9002e78ec78227a3134d4e704f42b685722fb4a9
HF <pixelplanetdev@gmail.com>
2022-04-28 15:47:32 +0200

    change PNG hash by changing / adding modification time chunk

--- src/js/file.js
+++ src/js/file.js
@@ -1,6 +1,9 @@
 import {
   debugLog,
 } from './misc';
+import {
+  changeModificationTime,
+} from './png';
 
 /*
  * change filename to random timestamp of the past year
@@ -11,40 +14,6 @@ export function anonFilename(file) {
   const filename = `${randomTimePastYear}.${file.name.split('.').slice(-1)}`;
   debugLog(`Changing the filename to "${filename}"`);
   return new File([file], filename, { type: file.type });
-}
-
-function changeRandomPixelOfPNG(file) {
-  return new Promise((resolve) => {
-    const mimetype = 'image/png';
-    const reader = new FileReader();
-    reader.addEventListener('load', () => {
-      const imgs = new Image();
-      imgs.onload = () => {
-        const cvs = document.createElement('canvas');
-        cvs.width = imgs.naturalWidth;
-        cvs.height = imgs.naturalHeight;
-        const canvas = cvs.getContext('2d');
-        canvas.drawImage(imgs, 0, 0);
-        const randX = Math.floor(Math.random() * cvs.width);
-        const randY = Math.floor(Math.random() * cvs.height);
-        const curPxl = canvas.getImageData(randX, randY, 1, 1).data;
-        let r = curPxl[0] + Math.floor(Math.random() * 7);
-        let g = curPxl[1] + Math.floor(Math.random() * 7);
-        let b = curPxl[2] + Math.floor(Math.random() * 7);
-        if (r > 255) r = curPxl[0] - Math.floor(Math.random() * 7);
-        if (g > 255) g = curPxl[1] - Math.floor(Math.random() * 7);
-        if (b > 255) b = curPxl[2] - Math.floor(Math.random() * 7);
-        canvas.fillStyle = `rgba(${r},${g},${b},255)`;
-        canvas.fillRect(randX, randY, 1, 1);
-        cvs.toBlob((blob) => {
-          const nFile = new File([blob], file.name, { type: mimetype });
-          return resolve(nFile);
-        }, mimetype, 0.9);
-      };
-      imgs.src = reader.result;
-    }, false);
-    reader.readAsDataURL(file);
-  });
 }
 
 /*
@@ -61,10 +30,7 @@ export function anonHash(file) {
       if (file.type === 'image/jpeg') {
         newFile = [reader.result.slice(0, -10), random, reader.result.slice(-2)];
       } else if (file.type === 'image/png') {
-        changeRandomPixelOfPNG(file).then((nFile) => {
-          resolve(nFile);
-        });
-        return;
+        newFile = changeModificationTime(reader.result);
       } else {
         newFile = [reader.result, random];
       }
---
+++ src/js/png.js
@@ -0,0 +1,140 @@
+/*
+ * PNG eferences:
+ * File structure:
+ * http://www.libpng.org/pub/png/spec/1.2/PNG-Structure.html
+ * Chunk specifications:
+ * http://www.libpng.org/pub/png/spec/1.2/PNG-Chunks.html
+ */
+
+import {
+  debugLog,
+} from './misc';
+
+/*
+ * calculate table for faster crc calculation
+ */
+function makeCRCTable() {
+  let c;
+  const crcTable = [];
+  for (let n = 0; n < 256; n++) {
+    c = n;
+    for (let k = 0; k < 8; k++) {
+      c = ((c & 1) ? (0xEDB88320 ^ (c >>> 1)) : (c >>> 1));
+    }
+    crcTable[n] = c;
+  }
+  return crcTable;
+}
+
+/*
+ * calculate 32bit crc for given string / array / Uint8Array
+ */
+export function crc32(str) {
+  const crcTable = window.crcTable || (window.crcTable = makeCRCTable());
+  let crc = 0 ^ (-1);
+  for (let i = 0; i < str.length; i++) {
+    crc = (crc >>> 8) ^ crcTable[(crc ^ str[i]) & 0xFF];
+  }
+  return (crc ^ (-1)) >>> 0;
+}
+
+/*
+ * get a random date at least one year and max two years ago
+ */
+function getRandomDateTwoYears() {
+  const yearMs = 365 * 24 * 60 * 60 * 1000;
+  const curtime = Date.now();
+  const randomTimePastTwoYear = curtime - yearMs - Math.floor(Math.random() * yearMs);
+  return new Date(randomTimePastTwoYear);
+}
+
+/*
+ * create tIME PNG chunk data out of given Date
+ */
+function createTIMEChunk(dateTime) {
+  const year = dateTime.getUTCFullYear();
+  const month = dateTime.getUTCMonth() + 1;
+  const day = dateTime.getUTCDate();
+  const hours = dateTime.getUTCHours();
+  const minutes = dateTime.getUTCMinutes();
+  const seconds = dateTime.getUTCSeconds();
+  const tIME = [
+    /* TYPE_TIME */
+    0x74, 0x49, 0x4d, 0x45,
+    year >>> 8, year & 0x00FF, month, day, hours, minutes, seconds,
+  ];
+  const crc = Number(crc32(tIME));
+  tIME.push(crc >>> 24);
+  tIME.push(crc >>> 16 & 0x000000FF);
+  tIME.push(crc >>> 8 & 0x000000FF);
+  tIME.push(crc & 0x000000FF);
+  return tIME.map((e) => Number(e));
+}
+
+/*
+ * various PNG chunk types
+ */
+const TYPE_TIME = 0x74494d45;
+const TYPE_IHDR = 0x49484452;
+const TYPE_IDAT = 0x49444154;
+const TYPE_IEND = 0x49454e44;
+
+/*
+ * search for first occurance of specific chunk type
+ */
+function searchForChunk(buffer, type) {
+  const view = new DataView(buffer);
+  const length = buffer.byteLength;
+  // first 8 bytes are signature
+  let i = 8;
+  while (i < length) {
+    const chunkLength = view.getUint32(i);
+    i += 4;
+    let chunkType = Number(view.getUint32(i));
+    if (chunkType === type) {
+      return i;
+    }
+    i += 4;
+    // skip crc
+    i += chunkLength + 4;
+  }
+  return null;
+}
+
+/*
+ * checks if PNG file has tIME chunk (modification time )
+ * and change it to a random time between 1 and 2 years ago.
+ * Creating new chunk if not available.
+ */
+export function changeModificationTime(buffer) {
+  const newDate = getRandomDateTwoYears();
+  const timeData = createTIMEChunk(newDate);
+  const tIMEPos = searchForChunk(buffer, TYPE_TIME);
+  try {
+    if (tIMEPos) {
+      /*
+       * tIME chunk exists, change it
+       */
+      const view = new DataView(buffer);
+      for (let i = 0; i < timeData.length; i += 1) {
+        view.setUint8(tIMEPos + i, timeData[i]);
+      }
+      return [buffer];
+    }
+    /*
+       * add new tIME chunk before first IDAT chunk
+       */
+    const iDATPos = searchForChunk(buffer, TYPE_IDAT) - 4;
+    const tIMEBuffer = Uint8Array.from([
+      // length minus crc and type
+      0x00, 0x00, 0x00, timeData.length - 8,
+      ...timeData,
+    ]);
+    return [buffer.slice(0, iDATPos), tIMEBuffer.buffer, buffer.slice(iDATPos)];
+  } catch (e) {
+    debugLog('Error when trying to change PNG modify time:', e);
+    return [buffer];
+  }
+}
+
+export default changeModificationTime;
